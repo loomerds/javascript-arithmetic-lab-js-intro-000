0c7af02cd08c809f48a74ab5c5bfc28f2eac8a35
Doug Loomer <loomerds@gmail.com>
2019-05-11 20:01:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
 fucntion add() {
-  
+
 }
 
 function subtract() {
-  
+
 }
 
 function multiply() {
-  
+
 }
 
 function divide() {
-  
+
 }
 
 function inc(n) {
-  
+
 }
 
 function dec(n) {
-  
+
 }
 
 makeInt(n) {
-  
+
 }
 
 preserveDecimal(n) {
-  
+
 }
